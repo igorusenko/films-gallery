08f4d06046e38b8d6dcc7f661fe95671a77271a1
Igor.Usenco <igorusenko2003@gmail.com>
2025-02-03 11:46:27 +0200

    environment fix

--- src/app/environment/environment.prod.ts
+++ src/app/environment/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.themoviedb.org/3/',
   apiKey: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZDU3MzE3MGFkZGM5MzBjODFmNDhhMDI3YTA0YTZjOCIsIm5iZiI6MTczODM5MzA0MS42NjYsInN1YiI6IjY3OWRjNWQxYTg5MzU5MTQxNTk1MzRkOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2Bryw0DdQrvOs791K9MZLCPbo7ekgVgcfUZ-pn4PA8Q',
-  imageUrl: 'https://image.tmdb.org/t/p/w220_and_h330_face/'
+  posterImageUrl: 'https://image.tmdb.org/t/p/w200/',
+  backdropImageUrl: 'https://image.tmdb.org/t/p/w500/',
+  logoImageUrl: 'https://image.tmdb.org/t/p/original/',
 };
